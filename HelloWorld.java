2d64683b5a5173bf895eeb5423066f6c8c8284fc
Bhushan <kakadbhushan@gmail.com>
2017-06-12 16:51:44 +0530

    added helloworld program

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Bhushan");
+		for(int i=0;i<6;i++)
+		{
+			System.out.println("hello world"+i);
+		}
+
+	}
+
+}
